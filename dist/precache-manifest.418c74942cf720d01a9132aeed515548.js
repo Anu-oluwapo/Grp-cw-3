a265a209c0337708fd0659a3194a1491105e5ad8
anu-oluwapo <abolarin.aanu@gmail.com>
2021-06-08 00:45:08 +0100

    Dist

---
+++ dist/precache-manifest.418c74942cf720d01a9132aeed515548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2705518cdb1ca2071381",
+    "url": "/css/app.b42d8893.css"
+  },
+  {
+    "revision": "3552587166005f975142d9d127887657",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2705518cdb1ca2071381",
+    "url": "/js/app.5aa4fd2e.js"
+  },
+  {
+    "revision": "4f8ab0093b2fe6342a31",
+    "url": "/js/chunk-vendors.18d3819a.js"
+  },
+  {
+    "revision": "3fd4db41b3b056be27356df25377c5e9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
